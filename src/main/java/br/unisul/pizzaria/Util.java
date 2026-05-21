64cc713b21f191b4fb5270512e297d904dd536f5
JAPON <santospeiterfernando@gmail.com>
2026-05-20 23:29:46 -0300

    feat(util): adiciona constantes do cabeçalho do sistema

---
+++ src/main/java/br/unisul/pizzaria/Util.java
@@ -0,0 +1,8 @@
+package br.unisul.pizzaria;
+
+public class Util {
+
+    public static final int LARGURA = 50;
+    public static final String EMPRESA = "PIZZARIA DA UNISUL LTDA.";
+    public static final String SISTEMA = "SISTEMA DE CONTROLE DE ESTOQUE";
+}
