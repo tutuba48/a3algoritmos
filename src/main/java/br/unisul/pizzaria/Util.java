a146d63b354b1d84537223a0c9d1b3f3f285e5af
JAPON <santospeiterfernando@gmail.com>
2026-05-20 23:31:15 -0300

    feat(util): helpers info/erro/mensagem com ícone customizado

--- src/main/java/br/unisul/pizzaria/Util.java
+++ src/main/java/br/unisul/pizzaria/Util.java
@@ -102,4 +102,19 @@ public class Util {
                 IconePizza.get(), opcoes, opcoes[0]);
         return resp == JOptionPane.YES_OPTION;
     }
+
+    public static void info(String mensagem) {
+        JOptionPane.showMessageDialog(null, mensagem, "Informação",
+                JOptionPane.PLAIN_MESSAGE, IconePizza.get());
+    }
+
+    public static void erro(String mensagem) {
+        JOptionPane.showMessageDialog(null, mensagem, "Erro",
+                JOptionPane.PLAIN_MESSAGE, IconePizza.get());
+    }
+
+    public static void mensagem(String titulo, String conteudo) {
+        JOptionPane.showMessageDialog(null, conteudo, titulo,
+                JOptionPane.PLAIN_MESSAGE, IconePizza.get());
+    }
 }
